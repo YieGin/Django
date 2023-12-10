30ad1c581164d87d1765646540b319fa6d752490
YieGin <pumasami7@gmail.com>
2023-12-10 19:06:36 +0100

    using Canocical URLS for models

--- Module01/mysite/blog/models.py
+++ Module01/mysite/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 class Post(models.Model):
     
@@ -9,6 +11,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     
     title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     body = models.TextField()
@@ -25,4 +28,8 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
+    
     
