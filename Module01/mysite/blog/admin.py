13b87e2d893826fe727478d46b4e17e9ca20850c
YieGin <pumasami7@gmail.com>
2023-12-10 21:17:05 +0100

    creating SEO urls for posts

--- Module01/mysite/blog/admin.py
+++ Module01/mysite/blog/admin.py
@@ -4,7 +4,7 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'name', 'slug', 'author', 'publish', 'status', ]
+    list_display = ['title', 'slug', 'author', 'publish', 'status', ]
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
     prepopulated_fields = {'slug': ('title',)}
--- Module01/mysite/blog/models.py
+++ Module01/mysite/blog/models.py
@@ -11,8 +11,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     
     title = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -28,8 +27,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-    
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
-    
+        return reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug,
+        ])
     
