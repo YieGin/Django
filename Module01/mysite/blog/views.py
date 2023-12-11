e79c9028bcd02d97f94c64cb34864dddb63bcf08
YieGin <pumasami7@gmail.com>
2023-12-11 13:22:55 +0100

    Building Class Based Views

--- Module01/mysite/blog/views.py
+++ Module01/mysite/blog/views.py
@@ -2,20 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
                              slug=post,
